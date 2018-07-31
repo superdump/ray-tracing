01fedaeacd1673f1f1529e046691fd5c6d495664
Robert Swain <robert.swain@gmail.com>
2018-07-31 20:40:22 +0200

    Add ray class and use it

---
+++ ray.hh
@@ -0,0 +1,18 @@
+#include "vec3.hh"
+
+#ifndef RAY_HH
+#define RAY_HH
+
+class ray {
+public:
+    vec3 A;
+    vec3 B;
+
+    ray() {}
+    ray(const vec3 &a, const vec3 &b) { A = a; B = b; }
+    vec3 origin() const { return A; }
+    vec3 direction() const { return B; }
+    vec3 point_at_parameter(float t) const { return A + t * B; }
+};
+
+#endif /* RAY_HH */
