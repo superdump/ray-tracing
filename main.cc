81fa14ff41eedc65e64f9867915394523907724e
Robert Swain <robert.swain@gmail.com>
2018-07-31 20:31:16 +0200

    Add vec3 class and use it

--- main.cc
+++ main.cc
@@ -1,18 +1,18 @@
 #include <iostream>
 
+#include "vec3.hh"
+
 int main() {
     int nx = 200;
     int ny = 100;
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     for (int j = ny - 1; j >= 0; --j) {
         for (int i = 0; i < nx; ++i) {
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2;
+            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2f);
 
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
+            int ir = int(255.99f * col[0]);
+            int ig = int(255.99f * col[1]);
+            int ib = int(255.99f * col[2]);
 
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
