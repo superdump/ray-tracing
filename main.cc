70a291ba2d1b2a98ccb561d4ed62f96b350e4398
Robert Swain <robert.swain@gmail.com>
2018-07-31 20:51:08 +0200

    Shade the sphere according to surface normals

--- main.cc
+++ main.cc
@@ -3,21 +3,27 @@
 #include "ray.hh"
 #include "vec3.hh"
 
-bool hit_sphere(const vec3& center, float radius, const ray& r) {
+float hit_sphere(const vec3& center, float radius, const ray& r) {
     vec3 oc = r.origin() - center;
     float a = dot(r.direction(), r.direction());
     float b = 2.0f * dot(oc, r.direction());
     float c = dot(oc, oc) - radius * radius;
     float discriminant = b * b - 4 * a * c;
-    return discriminant > 0.0f;
+    if (discriminant < 0.0f) {
+        return -1.0f;
+    } else {
+        return (-b - sqrtf(discriminant)) / (2.0f * a);
+    }
 }
 
 vec3 color(const ray& r) {
-    if (hit_sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f, r)) {
-        return vec3(1.0f, 0.0f, 0.0f);
+    float t = hit_sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f, r);
+    if (t > 0.0f) {
+        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0.0f, 0.0f, -1.0f));
+        return 0.5f * vec3(N.x() + 1.0f, N.y() + 1.0f, N.z() + 1.0f);
     }
     vec3 unit_direction = unit_vector(r.direction());
-    float t = 0.5f * (unit_direction.y() + 1.0f);
+    t = 0.5f * (unit_direction.y() + 1.0f);
     return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
 }
 
