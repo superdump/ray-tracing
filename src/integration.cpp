b6a780f3895e636780a089e34697b366336caa41
Robert Swain <robert.swain@gmail.com>
2018-08-26 15:15:17 +0200

    integration: pdf = 0.5

--- src/integration.cpp
+++ src/integration.cpp
@@ -2,14 +2,14 @@
 #include <iostream>
 
 inline float pdf(float x) {
-    return 0.5f * x;
+    return 0.5f;
 }
 
 int main() {
     int N = 1000000;
     float sum = 0.0f;
     for (int i = 0; i < N; ++i) {
-        float x = sqrtf(4.0f * drand48());
+        float x = 2.0f * drand48();
         sum += x * x / pdf(x);
     }
     std::cout << "I = " << sum / N << "\n";
