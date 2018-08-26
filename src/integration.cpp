c0363133ef7cd11fe373764ee0799e4e15e952b8
Robert Swain <robert.swain@gmail.com>
2018-08-26 15:17:43 +0200

    integration: pdf = (3/8)x^2

--- src/integration.cpp
+++ src/integration.cpp
@@ -2,14 +2,14 @@
 #include <iostream>
 
 inline float pdf(float x) {
-    return 0.5f;
+    return 3.0f * x * x / 8.0f;
 }
 
 int main() {
     int N = 1000000;
     float sum = 0.0f;
     for (int i = 0; i < N; ++i) {
-        float x = 2.0f * drand48();
+        float x = powf(8.0f * drand48(), 1.0f / 3.0f);
         sum += x * x / pdf(x);
     }
     std::cout << "I = " << sum / N << "\n";
